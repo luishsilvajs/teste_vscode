fc4cfe8c9fff7ee4ba4624f6aaa60084e0558cd2
Luis Henrique <105825523+luishsilvajs@users.noreply.github.com>
2025-12-05 17:32:07 +0000

    commit

---
+++ curso_JS-Udemy/node/moduleA.js
@@ -0,0 +1,3 @@
+this.ola = 'Olá, tudo bem?'
+exports.bemVindo = 'Bem vindo ao Node!'
+module.exports.ateLogo = 'Até a próxima'
---
+++ curso_JS-Udemy/node/moduleClient.js
@@ -0,0 +1,9 @@
+const moduleA = require('./moduleA')
+const moduleB = require('./moduleB')
+
+console.log(moduleA.ola)
+console.log(moduleA.bemVindo)
+console.log(moduleA.ateLogo)
+
+console.log(moduleB.bomDia)
+console.log(moduleB.boaNoite())

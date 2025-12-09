87a7da67e381c0d52b4ab53a383199768ffd2239
Luis Henrique <105825523+luishsilvajs@users.noreply.github.com>
2025-12-09 17:09:49 +0000

    commit_wndws

--- curso_JS-Udemy/node/pastaA/pastaB/accessModule.js
+++ curso_JS-Udemy/node/pastaA/pastaB/accessModule.js
@@ -1,6 +1,14 @@
 const moduleA = require('../../moduleA')
-
 console.log(moduleA.ola)
 
+// const saudacao = require('saudacao') ---- busca dentro de node_modules
+
+const http = require ('http')
+http.createServer((req, red ) =>{
+    res.write('Bom dia')
+    res.end()
+}).listen(8080)
+
 const c = require('./pastaC/index')
+const { readdirSync } = require('fs')
 console.log(c.ola2)

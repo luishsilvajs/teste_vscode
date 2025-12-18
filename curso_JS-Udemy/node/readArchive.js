3c8eec71b065c12d6d83fc8ebfc37c809b5836e3
Luis Henrique <105825523+luishsilvajs@users.noreply.github.com>
2025-12-18 14:19:40 +0000

    commit_wndws

---
+++ curso_JS-Udemy/node/readArchive.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const path = __dirname + '/archive.json'
+
+// leitura sincrona
+const conteudo = fs.readFileSync(path, 'utf-8')
+// console.log(conteudo)
+
+// leitura assincrona
+fs.readFile(path, 'utf-8', (error, conteudo) => {
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}: ${config.db.porta}`)
+})
+
+const config = require('./archive.json')
+console.log(config.db)
+
+fs.readdir(__dirname, (error, conteudo) => {
+    console.log('arquivos de uma pasta:')
+    console.log(conteudo)
+})
